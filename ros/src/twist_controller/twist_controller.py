83f9c57ecc03253385e635b41e941098ea1398fd
Matthias Bartelmeß <mba@fourplusone.de>
2018-03-18 18:36:56 +0100

    Add PID controller, for acceleration and brake

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,5 @@
 from yaw_controller import YawController
+from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -6,14 +7,24 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, car_properties):
-        # TODO: Implement
         self.yaw_controller = YawController(car_properties)
-        pass
+        
+        self.acceleration_controller = PID(1.,0.,0., mn=-1., mx=1.)
 
     def control(self, linear_v, angular_v, current_v, dbw_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         
+        accelerate = self.acceleration_controller.step(linear_v - current_v ,1./50.)
+        
+        
+        brake = 0.
+        throttle = 0.
+        if accelerate > 0:
+            throttle = accelerate
+        else:
+            brake = -accelerate
+        
         steer = self.yaw_controller.get_steering(linear_v, angular_v, current_v)
         
-        return 0.3, 0., steer
+        return throttle, brake, steer
