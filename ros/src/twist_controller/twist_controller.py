7e062cad9fe05f62cb10e567d1b9c2a4b9b87483
Antonio Isas <antonioisas87@gmail.com>
2018-03-26 22:23:28 -0600

    Additional brake logic to keep speed under limit

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,5 +1,7 @@
 from yaw_controller import YawController
 from pid import PID
+#from rospy import get_param
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -11,10 +13,12 @@ class Controller(object):
         
         self.acceleration_controller = PID(0.1,3.e-3,4.e-3, mn=-1., mx=1.)
 
+        self.car_properties = car_properties
+        self.max_velocity = (rospy.get_param('/waypoint_loader/velocity') * 1000.) / (60.*60.) # velocity in mps
+
     def control(self, linear_v, angular_v, current_v, dbw_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        
         if not dbw_enabled:
             self.acceleration_controller.reset()
             return 0., 0., 0.
@@ -34,4 +38,9 @@ class Controller(object):
         
         steer = self.yaw_controller.get_steering(linear_v, angular_v, current_v)
         
+        if current_v>=self.max_velocity*.98:
+            throttle = 0
+            brake += 2
+            
+        brake = brake*self.car_properties.vehicle_mass*self.car_properties.accel_limit*self.car_properties.wheel_radius
         return throttle, brake, steer
